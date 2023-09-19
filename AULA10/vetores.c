673b65aee6fb7d68f3bd5497adb5f66a3f362560
Vinícius Giroti <121032547+Vinicius2901@users.noreply.github.com>
2023-09-19 16:22:06 -0300

    Versão melhorada com funções de matriz

--- AULA10/vetores.c
+++ AULA10/vetores.c
@@ -28,10 +28,10 @@ void mostra_vetor_2(int x[]/*o vetor é um ponteiro*/, int k, char nome[])
 void selecion_sort(int x[], int k)
 {
     int i, j;
-    for (i = 0; i < MAX-1; i++)
+    for (i = 0; i <k-1; i++)
     {
         int i_menor = i;
-        for(j = i+1; j < MAX; j++)
+        for(j = i+1; j < k; j++)
         {
             if(x[j] < x[i_menor])
             {
@@ -51,7 +51,7 @@ void bubble_sort(int x[], int k)
     {
         trocou = 0;
 
-        for (i = 0; i < MAX-1; i++)
+        for (i = 0; i < k-1; i++)
         {
             if(x[i] > x[i+1])
             {
@@ -63,4 +63,31 @@ void bubble_sort(int x[], int k)
         }
     }
     while (trocou);
+}
+
+void gera_valores_matriz (int lin, int col, int x[lin][col])
+{
+    int i, j;
+    for(i = 0; i < lin; i++)
+    {
+        for(j = 0; j < col; j++)
+        {
+            x[i][j] = rand() % 10000;
+        }
+    }
+}
+
+void mostra_matriz (int lin, int col, int x[lin][col])
+{
+    int i, j;
+    for(i = 0; i < lin; i++)
+    {
+        for(j = 0; j < col; j++)
+        {
+            printf("%d\t", x[i][j]); //  \t funciona como tabulação e seria o "TAB", para deixar alinhado.
+        }
+        printf("\n");
+    }
+    printf("----------------------------------------------------------------------------------------------------------------------------------------------------------------\n");
+    return 0;
 }
