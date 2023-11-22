9cb3e9a778b6c2806c9604b0e7a5b8c72a541f32
Vinícius Giroti <121032547+Vinicius2901@users.noreply.github.com>
2023-11-22 10:11:47 -0300

    Create q2.c

---
+++ PROVA/q2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *tira_letra( char *s, char c );
+
+int main()
+{
+    char s[20];
+    printf("Digite uma palavra: ");
+    scanf("%s", s );
+    char key;
+    printf("Digite uma letra: ");
+    scanf(" %c", &key );
+    
+    char *r = tira_letra( s, key );
+    
+    printf("A palavra '%s' sem '%c' eh: %s\n", s, key, r );
+    
+    return 0;
+    
+}
+
+char *tira_letra( char *s, char c )
+{
+    int i, aux = 0;
+    int n = strlen( s );
+    char *strrem = malloc( n+1 );
+    for( i = 0; i < n+1; i++ )
+        if( s[i] != c )
+        {
+            strrem[aux] = s[i];
+            aux++;
+            strrem = realloc( strrem, aux );
+        }
+    strrem = realloc(strrem, aux+1 );
+    return strrem;
+}
