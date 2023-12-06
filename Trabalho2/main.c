f296197523e92456c34a6c389003ee1ff375eb99
Vinícius Giroti <121032547+Vinicius2901@users.noreply.github.com>
2023-12-06 15:22:06 -0300

    Update main.c

--- Trabalho2/main.c
+++ Trabalho2/main.c
@@ -1,33 +1,37 @@
 #include "trabalho.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main()
 {
     Evento *v = NULL; // Declarção do vetor de eventos.
     int n = 0;
-    int i, opt;
+    int i, opt, conf;
 
     carrega_registro( v, n, "eventos.txt" ); // Carrega os registros dos eventos já registrados antes da execução do código.
 
     do
     {
         menu();
+        n = 1;
+        v = malloc( sizeof(Evento) * n );
+        v[0].event.ano = 2023;
+        v[0].event.mes = 12;
+        v[0].event.dia = 25;
+        v[0].inicio.hora = 0;
+        v[0].inicio.min = 10;
+        v[0].fim.hora = 1;
+        v[0].fim.min = 0;
+        strcpy( v[0].info, "Natal" );
+        strcpy( v[0].local, "Casa" );
+        scanf("%i", &opt );
         switch ( opt )
         {
         case 1:
-            v = malloc( sizeof(Evento) * n );
-            v[0].event.ano = 2023;
-            v[0].event.mes = 12;
-            v[0].event.dia = 25;
-            v[0].inicio.hora = 0;
-            v[0].inicio.min = 0;
-            v[0].fim.hora = 1;
-            v[0].fim.hora = 0;
-
-            insere_evento( v, n );
-            if( insere_evento( v, n ) )
-                printf("Nao tem como inserir evento");
+            conf = insere_evento( v, n );
+            if( conf )
+                printf("\nNao tem como inserir evento\n");
             break;
         case 2:
             if( n == 0 )
@@ -63,5 +67,5 @@ int main()
         }
     } while ( opt != 6 );
     
-
+    return 0;
 }
